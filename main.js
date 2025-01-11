fbe2d3973c017499bbd05c0d31fc8e7360f40d46
flo <flo.schwend@gmail.com>
2025-01-11 15:30:34 +0900

    next step to get all items

--- main.js
+++ main.js
@@ -1,106 +1,110 @@
+const apikey = "...";
+
 String.prototype.addQuery = function (obj) {
-    return (this == "" ? "" : `${this}?`) + Object.entries(obj).flatMap(([k, v]) => Array.isArray(v) ? `${k}=` + v.map(e => `${encodeURIComponent(e)}`) : `${k}=${encodeURIComponent(v)}`).join("&");
+  return (this == "" ? "" : `${this}?`) + Object.entries(obj).flatMap(([k, v]) => Array.isArray(v) ? `${k}=` + v.map(e => `${encodeURIComponent(e)}`) : `${k}=${encodeURIComponent(v)}`).join("&");
+}
+
+eval(UrlFetchApp.fetch('https://raw.githubusercontent.com/jmespath/jmespath.js/refs/heads/master/jmespath.js').getContentText());
+
+function fetchAssignments() {
+
+  const url = "https://api.wanikani.com/v2/assignments"
+  const params = {
+    srs_stages: Array(9).fill().map((element, index) => index + 1),
+    subject_types: ["kanji","radical","vocabulary"]
+  };
+
+  const startUrl = url.addQuery(params).toString();
+  var assignments = fetchAssignmentsPage(startUrl);
+
+  var subjectIds = jmespath.search(assignments, "[*].subject_id");
+  var subjects = fetchSubjects(subjectIds);
+
+  var spreadSheet = SpreadsheetApp.getActiveSpreadsheet();
+  var sheet = spreadSheet.getSheetByName("Kanji");
+  spreadSheet.setActiveSheet(sheet);
+
+  sheet.clear();
+  sheet.appendRow(["Kanji", "Meaning", "Reading"]);
+
+  for (const subject of subjects.filter(s => s.type == 'kanji')) {
+
+    const character = subject.data.slug;
+    const meaning = jmespath.search(subject.data, 'meanings[?primary==`true`].meaning | [0]')
+    const reading = jmespath.search(subject.data, 'readings[?primary==`true`].reading | [0]')
+
+    const rowData = [character, meaning, reading];
+    Logger.log("Appending row: " + rowData)
+
+    sheet.appendRow(rowData);
   }
-  
-  eval(UrlFetchApp.fetch('https://raw.githubusercontent.com/jmespath/jmespath.js/refs/heads/master/jmespath.js').getContentText());
-  
-  function fetchKanjis() {
-  
-    url = "https://api.wanikani.com/v2/assignments"
-    const params = {
-      srs_stages: Array(9).fill().map((element, index) => index + 1),
-      subject_types: "kanji"
+}
+
+function fetchSubjects(ids) {
+
+  var subjects = [];
+
+  const chunkSize = 300;
+  for (let i = 0; i < ids.length; i += chunkSize) {
+    const chunk = ids.slice(i, i + chunkSize);
+    subjects = subjects.concat(fetchSubjectsPage(chunk));
+  }
+
+  Logger.log("Found total subjects: " + subjects.length)
+
+  return subjects;
+}
+
+function fetchSubjectsPage(ids) {
+
+  const url = "https://api.wanikani.com/v2/subjects"
+  const params = {
+    ids: ids.map((element, index) => element),
+  };
+
+  const pageUrl = url.addQuery(params).toString();
+
+  const data = fetchAndParseUrl(pageUrl);
+  var subjects = jmespath.search(data, 'data[*].{type: object, data: data}');
+
+  Logger.log("Found subjects in page: " + subjects.length)
+
+  return subjects;
+}
+
+
+function fetchAssignmentsPage(url) {
+
+  const data = fetchAndParseUrl(url);
+
+  var assignments = jmespath.search(data, 'data[*].data');
+
+  Logger.log("Found assignments in page: " + assignments.length)
+
+  if (data.pages.next_url?.length > 0) {
+    assignments = assignments.concat(fetchAssignmentsPage(data.pages.next_url));
+  }
+
+  return assignments;
+}
+
+function fetchAndParseUrl(url) {
+
+  Logger.log("Fetching: " + url);
+
+  try {
+    const options = {
+      headers: { 'Wanikani-Revision': '20170710', Authorization: 'Bearer ' + apikey }
     };
-  
-    startUrl = url.addQuery(params).toString();
-    subjects = fetchKanjiIdsPage(startUrl);
-  
-    var ids = jmespath.search(subjects, "[*].subject_id");
-    var subjects = fetchSubjects(ids);
-  
-    var spreadSheet = SpreadsheetApp.getActiveSpreadsheet();
-    var sheet = spreadSheet.getSheetByName("Kanji");
-    spreadSheet.setActiveSheet(sheet);
-  
-    sheet.clear();
-    sheet.appendRow(["Kanji", "Meaning", "Reading"]);
-  
-    for (const subject of subjects) {
-  
-      var character = subject.slug;
-      var meaning = jmespath.search(subject, 'meanings[?primary==`true`].meaning | [0]')
-      var reading = jmespath.search(subject, 'readings[?primary==`true`].reading | [0]')
-  
-      var rowData = [character, meaning, reading];
-      Logger.log("Appending row: " + rowData)
-  
-      sheet.appendRow(rowData);
-    }
+
+    const response = UrlFetchApp.fetch(url, options);
+    const data = JSON.parse(response.getContentText());
+
+    return data;
+
+  } catch (f) {
+    Logger.log(f.message);
   }
-  
-  function fetchSubjects(ids) {
-  
-    subjects = [];
-  
-    const chunkSize = 500;
-    for (let i = 0; i < ids.length; i += chunkSize) {
-      const chunk = ids.slice(i, i + chunkSize);
-      subjects = subjects.concat(fetchSubjectsPage(chunk));
-    }
-  
-    Logger.log("Found total subjects: " + subjects.length)
-  
-    return subjects;
-  }
-  
-  function fetchSubjectsPage(ids) {
-  
-    url = "https://api.wanikani.com/v2/subjects"
-    const params = {
-      ids: ids.map((element, index) => element),
-    };
-  
-    pageUrl = url.addQuery(params).toString();
-  
-    const data = fetchAndParseUrl(pageUrl);
-    var subjects = jmespath.search(data, 'data[*].data');
-  
-    Logger.log("Found subjects in page: " + subjects.length)
-  
-    return subjects;
-  }
-  
-  function fetchKanjiIdsPage(url) {
-  
-    const data = fetchAndParseUrl(url);
-  
-    subjects = jmespath.search(data, 'data[*].data');
-  
-    if (data.pages.next_url?.length > 0) {
-      subjects = subjects.concat(fetchKanjiIdsPage(data.pages.next_url));
-    }
-  
-    return subjects;
-  }
-  
-  function fetchAndParseUrl(url) {
-  
-    Logger.log("Fetching: " + url);
-  
-    try {
-      const options = {
-        headers: { 'Wanikani-Revision': '20170710', Authorization: 'Bearer <API-KEY>' }
-      };
-  
-      const response = UrlFetchApp.fetch(url, options);
-      const data = JSON.parse(response.getContentText());
-  
-      return data;
-  
-    } catch (f) {
-      Logger.log(f.message);
-    }
-  
-  }
-  
-  
+
+}
+
