d9752a21528e09e54531859ae79a8ec5dafba713
flo <flo.schwend@gmail.com>
2025-01-15 10:56:13 +0900

    continue

--- main.js
+++ main.js
@@ -71,7 +71,13 @@ function fillInRadicals(subjects) {
     const name = subject.data.slug;
     const meaning = jmespath.search(subject.data, 'meanings[?primary==`true`].meaning | [0]')
 
+    Logger.log(subject.data.character_images[0].url);
+
     var svgCode = UrlFetchApp.fetch(subject.data.character_images[0].url).getContentText(); 
+    svgCode = Utilities.base64Encode(svgCode);
+
+    svgCode = "PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI0MDAiIGhlaWdodD0iNDAwIiB2aWV3Qm94PSIwIDAgMTI0IDEyNCIgZmlsbD0ibm9uZSI+DQo8cmVjdCB3aWR0aD0iMTI0IiBoZWlnaHQ9IjEyNCIgcng9IjI0IiBmaWxsPSIjRjk3MzE2Ii8+DQo8cGF0aCBkPSJNMTkuMzc1IDM2Ljc4MThWMTAwLjYyNUMxOS4zNzUgMTAyLjgzNCAyMS4xNjU5IDEwNC42MjUgMjMuMzc1IDEwNC42MjVIODcuMjE4MUM5MC43ODE4IDEwNC42MjUgOTIuNTY2NCAxMDAuMzE2IDkwLjA0NjYgOTcuNzk2NkwyNi4yMDM0IDMzLjk1MzRDMjMuNjgzNiAzMS40MzM2IDE5LjM3NSAzMy4yMTgyIDE5LjM3NSAzNi43ODE4WiIgZmlsbD0id2hpdGUiLz4NCjxjaXJjbGUgY3g9IjYzLjIxMDkiIGN5PSIzNy41MzkxIiByPSIxOC4xNjQxIiBmaWxsPSJibGFjayIvPg0KPHJlY3Qgb3BhY2l0eT0iMC40IiB4PSI4MS4xMzI4IiB5PSI4MC43MTk4IiB3aWR0aD0iMTcuNTY4NyIgaGVpZ2h0PSIxNy4zODc2IiByeD0iNCIgdHJhbnNmb3JtPSJyb3RhdGUoLTQ1IDgxLjEzMjggODAuNzE5OCkiIGZpbGw9IiNGREJBNzQiLz4NCjwvc3ZnPg=="
+
     var converterUrl = PropertiesService.getScriptProperties().getProperty("converter_url");
 
     // Make a POST request with a JSON payload. 
@@ -81,14 +87,21 @@ function fillInRadicals(subjects) {
       contentType: 'application/json', 
       payload: JSON.stringify(data), 
     }; 
-    var pngBytes = UrlFetchApp.fetch(converterUrl, options).getBlob().getBytes();
+
+    //var pngResponse = UrlFetchApp.fetch(converterUrl, options);
+    var pngResponse = UrlFetchApp.fetch(
+      'https://developers.google.com/adwords/scripts/images/reports.png');
+
+    var binData = pngResponse.getContent();
+    //var pngBytes = pngBlob.getBytes();
+
 
     const rowData = [name, meaning];
-    Logger.log("Appending row: " + rowData)
+    Logger.log("Appending row: " + rowData);
 
     sheet.appendRow(rowData);
     
-    var blob = Utilities.newBlob(pngBytes, 'image/png');
+    var blob = Utilities.newBlob(binData, 'image/png', 'test');
     var image = sheet.insertImage(blob, sheet.getLastColumn(), sheet.getLastRow());
   }
 }
