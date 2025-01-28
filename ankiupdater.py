81f8c8d544839752100e9dce0303a5e94a31a69d
flo <flo.schwend@gmail.com>
2025-01-29 08:46:12 +0900

    script to distribute anki cards on the same date, belonging to the same note

---
+++ ankiupdater.py
@@ -0,0 +1,50 @@
+from anki.collection import Collection
+from datetime import datetime, timedelta, date
+import itertools
+from collections import namedtuple
+
+GroupKey = namedtuple("GroupKey", ["note_id", "due_date"])
+
+class CardInfo(object):
+    pass
+
+def get_card_info(col, card_id):
+    card = col.get_card(card_id)
+    duedays = card.due - col.sched.today
+    info = CardInfo()
+    info.card_id = card_id
+    info.due = card.due
+    info.due_date = date.today() + timedelta(days=duedays)
+    info.note_id = card.note().id
+    return info
+
+def get_anki_date(col, due):
+    duedays = due - col.sched.today
+    return date.today() + timedelta(days=duedays)
+
+def update_cards(col, card_group):
+    days = 0
+    for card in card_group:
+        print(card.note_id)
+        if(days > 0):
+            new_due = card.due + days
+            print("Old: {old_date} ({old_due}), new: {new_date} ({new_due})".format(old_date = card.due_date, new_date = get_anki_date(col, new_due), old_due = card.due, new_due = new_due))
+            
+            ankicard = col.get_card(card.card_id)
+            ankicard.due = new_due
+            col.update_card(ankicard)
+            
+        days += 1
+
+col = Collection("C:\\Users\\...\\AppData\\Roaming\\Anki2\\User 1\\collection.anki2")
+card_ids = col.find_cards("-is:new -is:suspended")
+cards = [get_card_info(col, v) for v in card_ids]
+
+groups = [(k, list(g)) for k, g in itertools.groupby(cards, lambda x: GroupKey(x.note_id, x.due_date))]
+
+duplicates = [(k, g) for k, g in groups if len(g) >= 2]
+
+print("{0} duplicates found".format(len(duplicates)))
+
+for key, group in duplicates:
+    update_cards(col, group)
