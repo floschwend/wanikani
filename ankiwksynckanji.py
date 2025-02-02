17108d3f6b0eda69e75939bb5a83e12ecc15a45b
flo <flo.schwend@gmail.com>
2025-02-02 14:50:34 +0900

    fetch kanji details

--- ankiwksynckanji.py
+++ ankiwksynckanji.py
@@ -10,13 +10,9 @@ import yaml
 
 config = yaml.safe_load(open("config.yaml"))
 
-# Get Kanjis from WK
-
-def fetchAndParseUrl(url):
+def fetchAndParseUrl(url, params):
 
     print("Fetching: {url}".format(url= url))
-
-    params = {"srs_stages": ','.join(str(i) for i in range(1,10)), "subject_types":"kanji"}
     
     headers = {"Wanikani-Revision": "20170710", "Authorization": "Bearer {key}".format(key= config['waniKaniKey'])}
 
@@ -29,46 +25,66 @@ def fetchAndParseUrl(url):
     except Exception as inst:
         print("Exception in fetchAndParseUrl: {msg}".format(msg = inst))
     
-def fetchAssignmentsPage(url, a):  
-    data = fetchAndParseUrl(url)
+def fetchAssignmentsPage(url, params):  
+    data = fetchAndParseUrl(url, params)
 
     assignments = jmespath.search('data[*].data', data)
 
     print("Found assignments in page: {number}".format(number= len(assignments)))
 
     next = data["pages"]["next_url"]
-    if a == 1:
-        return assignments
-    #if next is not None:
-    #    assignments = assignments.append(fetchAssignmentsPage(next))
-    assignments = assignments.append(fetchAssignmentsPage(url, 1))
+    if next is not None:
+        assignments = assignments.append(fetchAssignmentsPage(next, {}))    #empty params as the URL already contains them
 
     return assignments
 
-fetchAssignmentsPage("https://api.wanikani.com/v2/assignments", 2)
-
-'''
-
 def fetchAssignments():
 
-  url = "https://api.wanikani.com/v2/assignments"
-  params = {
-    srs_stages: Array(9).fill().map((element, index) => index + 1),
-    subject_types: ["kanji","radical","vocabulary"]
-  };
+    url = "https://api.wanikani.com/v2/assignments"
+    params = {"srs_stages": ','.join(str(i) for i in range(1,10)), "subject_types":"kanji"}
 
-  startUrl = url.addQuery(params).toString()
-  assignments = fetchAssignmentsPage(startUrl)
+    assignments = fetchAssignmentsPage(url, params)
 
-  subjectIds = jmespath.search(assignments, "[*].subject_id")
-  subjects = fetchSubjects(subjectIds)
+    subjectIds = jmespath.search("[*].subject_id", assignments)
+    subjects = fetchSubjects(subjectIds)
 
-  fillInKanjis(subjects)
+    print(subjects)
+    #fillInKanjis(subjects)
+
+def fetchSubjects(ids):
+
+    subjects = []
+
+    chunkSize = 300
+    for i in range(0, len(ids), chunkSize):
+        inclfrom = i
+        exclto = min(i+chunkSize, len(ids))
+        subids = list(ids[inclfrom:exclto])
+        subjdata = fetchSubjectsPage(subids)
+        subjects.append(subjdata)
+
+    print("Found total subjects: subjtotal".format(subjtotal = len(subjects)))
+
+    return subjects
 
 
+def fetchSubjectsPage(ids):
+
+    url = "https://api.wanikani.com/v2/subjects"
+    params = {"ids":",".join(map(str, ids))}
+
+    data = fetchAndParseUrl(url, params)
+    subjects = jmespath.search('data[*].{type: object, data: data}', data)
+
+    print("Found subjects in page: {subjpage}".format(subjpage=len(subjects)))
+
+    return subjects
+
+
+# Get Kanjis from WK
+fetchAssignments()
 
 # Open Anki Collection
 
 
 #
-'''
