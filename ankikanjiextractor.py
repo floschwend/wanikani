2947b427842c7c5fea162426df573605fb0b39e2
flo <flo.schwend@gmail.com>
2025-04-10 17:36:04 +0900

    update path

--- ankikanjiextractor.py
+++ ankikanjiextractor.py
@@ -7,7 +7,7 @@ def getNoteInfo(col, note_id, key):
     return note[key]
 
 # Open Anki Collection
-col = Collection("{userhome}\\AppData\\Roaming\\Anki2\\User 1\\collection.anki2".format(userhome = Path.home()))
+col = Collection("{userhome}\\AppData\\Roaming\\Anki2\\Flo\\collection.anki2".format(userhome = Path.home()))
 
 kanji_note_ids = col.find_notes("Deck:Kanji")
 existing_kanjislugs = [getNoteInfo(col, v, "Character") for v in kanji_note_ids]
