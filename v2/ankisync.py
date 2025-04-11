af551be4c6ff588ce60644728bd51d99c543d874
flo <flo.schwend@gmail.com>
2025-04-11 14:52:12 +0900

    fixed some typos and git ignore

--- v2/ankisync.py
+++ v2/ankisync.py
@@ -1,39 +1,44 @@
 import ankilib, wksync
 
-# Sync Nilay
-col = ankilib.open_collection("Nilay")
-subjects = wksync.fetchSubjects("kanji,radical,vocabulary")
-kanjis = [v for v in subjects if v["type"] == "kanji"]
-radicals = [v for v in subjects if v["type"] == "radical"]
-vocab = [v for v in subjects if v["type"] == "vocabulary"]
+def syncNilay():
+    col = ankilib.open_collection("Nilay")
+    subjects = wksync.fetchSubjects("kanji,radical,vocabulary")
+    kanjis = [v for v in subjects if v["type"] == "kanji"]
+    radicals = [v for v in subjects if v["type"] == "radical"]
+    vocab = [v for v in subjects if v["type"] == "vocabulary"]
 
-kanji_note_ids = col.find_notes("Deck:Kanji")
-existing_kanjislugs = [wksync.getNoteInfo(col, v, "Character") for v in kanji_note_ids]
-ankilib.createMissingKanji(col, kanjis, existing_kanjislugs, radicals)
+    kanji_note_ids = col.find_notes("Deck:Kanji")
+    existing_kanjislugs = [wksync.getNoteInfo(col, v, "Character") for v in kanji_note_ids]
+    ankilib.createMissingKanji(col, kanjis, existing_kanjislugs, radicals)
 
-radical_note_ids = col.find_notes("Deck:Radicals")
-existing_radicalnames = [wksync.getNoteInfo(col, v, "Name") for v in radical_note_ids]
-ankilib.createMissingRadicals(col, radicals, existing_radicalnames, kanjis)
+    radical_note_ids = col.find_notes("Deck:Radicals")
+    existing_radicalnames = [wksync.getNoteInfo(col, v, "Name") for v in radical_note_ids]
+    ankilib.createMissingRadicals(col, radicals, existing_radicalnames, kanjis)
 
-vocab_note_ids = col.find_notes("Deck:Vocab note:VocabWithKanji")
-existing_vocab = [wksync.getNoteInfo(col, v, "Kanji") for v in vocab_note_ids]
-#ankilib.createMissingRadicals(col, radicals, existing_radicalnames, kanjis) 
+    vocab_note_ids = col.find_notes("Deck:Vocab note:VocabWithKanji")
+    existing_vocab = [wksync.getNoteInfo(col, v, "Word") for v in vocab_note_ids]
+    #ankilib.createMissingRadicals(col, radicals, existing_radicalnames, kanjis) 
 
-col.close()
+    col.close()
+
+
+def syncFlo():
+    col = ankilib.open_collection("Flo")
+
+    subjects = wksync.fetchSubjects("kanji,radical")
+    kanjis = [v for v in subjects if v["type"] == "kanji"]
+    radicals = [v for v in subjects if v["type"] == "radical"]
+
+    kanji_note_ids = col.find_notes("Deck:Kanji")
+    existing_kanjislugs = [wksync.getNoteInfo(col, v, "Character") for v in kanji_note_ids]
+    ankilib.createMissingKanji(col, kanjis, existing_kanjislugs, radicals)
+
+    radical_note_ids = col.find_notes("Deck:Radicals")
+    existing_radicalnames = [wksync.getNoteInfo(col, v, "Name") for v in radical_note_ids]
+    ankilib.createMissingRadicals(col, radicals, existing_radicalnames, kanjis)
+
+    col.close()
+
 
 # Sync Flo
-col = ankilib.open_collection("Flo")
-
-subjects = wksync.fetchSubjects("kanji,radical")
-kanjis = [v for v in subjects if v["type"] == "kanji"]
-radicals = [v for v in subjects if v["type"] == "radical"]
-
-kanji_note_ids = col.find_notes("Deck:Kanji")
-existing_kanjislugs = [wksync.getNoteInfo(col, v, "Character") for v in kanji_note_ids]
-ankilib.createMissingKanji(col, kanjis, existing_kanjislugs, radicals)
-
-radical_note_ids = col.find_notes("Deck:Radicals")
-existing_radicalnames = [wksync.getNoteInfo(col, v, "Word") for v in radical_note_ids]
-ankilib.createMissingRadicals(col, radicals, existing_radicalnames, kanjis)
-
-col.close()
+syncFlo()
